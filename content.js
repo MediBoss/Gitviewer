2d4d187aa53d9a2af90f747b33692a6c436186f0
Medi Assumani <mediassumani49@gmail.com>
2019-01-08 13:43:42 -0800

    [Add] content-script to track the user activities ONLY on github.com

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-console.log("Git Viewer extension");
+
+// Returns the github handle of the visited gihub pages
+(function () {
+  var user_url = window.location.pathname.split('/');
+  const user_github_handle = user_url[1];
+  console.log(user_github_handle);
+}());
