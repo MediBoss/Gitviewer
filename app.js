ed09b0f007ee0948290405ea10346eab3071cfe5
Medi Assumani <mediassumani49@gmail.com>
2019-01-10 15:45:13 -0800

    [fix] refarctor socket server

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Mini socket server to interact with the chrome extension
+
 var express = require("express");
 var app = express()
 var http = require("http").Server(app);
@@ -6,33 +8,12 @@ var path = require('path');
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
-// app.get('/', function(request, response){
-//   response.sendFile(path.join(__dirname+'/index.html'))
-// });
 
 io.on('connection', function(socket){
-  socket.on("chat message", function(data){
+  socket.on("github event", function(data){
     console.log('data : '+ data)
   })
 });
 
 // SERVER BOOTING UP
 http.listen(port);
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-//
-// // app.get('/', function(req, res){
-// //   res.sendFile(__dirname + '/index.html');
-// // });
-//
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log('message: ' + msg);
-//   });
-// });
-//
-//
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
