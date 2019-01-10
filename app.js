ddd9798ecb725cf17bd334f9d096882bade5a3a4
Medi Assumani <mediassumani49@gmail.com>
2019-01-10 15:42:29 -0800

    separated client and server

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-// var express = require("express");
-// var app = express()
-// var http = require("http").Server(app);
-// var io = require('socket.io')(http);
-// var path = require('path');
-// const port = process.env.PORT || 3000;
-// app.use(express.static("public"));
-//
-// app.get('/', function(request, response){
-//   response.sendFile(path.join(__dirname+'/home.html'))
-// });
-//
-// io.on('connection', function(socket){
-//   socket.on("clientEvent", function(data){
-//     console.log('data : '+ data)
-//   })
-// });
-//
-// // SERVER BOOTING UP
-// http.listen(port);
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require("express");
+var app = express()
+var http = require("http").Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+const port = process.env.PORT || 3000;
+app.use(express.static("public"));
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
+// app.get('/', function(request, response){
+//   response.sendFile(path.join(__dirname+'/index.html'))
 // });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
+  socket.on("chat message", function(data){
+    console.log('data : '+ data)
+  })
 });
 
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+// SERVER BOOTING UP
+http.listen(port);
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+//
+// // app.get('/', function(req, res){
+// //   res.sendFile(__dirname + '/index.html');
+// // });
+//
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     console.log('message: ' + msg);
+//   });
+// });
+//
+//
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
