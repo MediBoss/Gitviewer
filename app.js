8cc9b6cabf01428379f43cc90974c7c7d9a263e6
Medi Assumani <mediassumani49@gmail.com>
2019-01-10 17:45:34 -0800

    [Add] mongoose

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // Mini socket server to interact with the chrome extension
 
 var express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 var app = express()
 var http = require("http").Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
+
 
 
 io.on('connection', function(socket){
