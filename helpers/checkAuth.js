2a478de7bf9d4c71c70d967a4a697dd0de88d4b6
Medi Assumani <mediassumani49@gmail.com>
2019-02-23 14:37:21 -0800

    [Add] authorization middleware

--- helpers/checkAuth.js
+++ helpers/checkAuth.js
@@ -2,15 +2,25 @@
 
 const jwt = require('jsonwebtoken')
 const superagent = require('superagent')
+
 var checkAuth = (request, response, next) => {
+  console.log("Authenticating User");
   if (typeof request.cookies.gvToken === "undefined" || request.cookies.gvToken === null) {
     console.log("No token received");
     request.user = null;
+  } else {
+    var token = request.cookies.gvToken;
+    console.log("check auth token " + token);
+    superagent
+      .get('https://api.github.com/user')
+      .set('Authorization', 'token ', + token)
+      .then(result => {
+        const user = new User(result.body)
+        request.user = user
+      }).catch( (error) => {
+        return response.status(400).send({ err: error})
+      })
   }
-
-  var token = request.cookies.gvToken;
-  console.log("check auth token " + token);
-
   next();
 };
 
